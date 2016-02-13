6daa74b5002033015193d7c7e7128e8d7c8e6400
aronkatona <katonaron@gmail.com>
2016-02-13 22:18:53 +0100

    springboot jms test

---
+++ src/main/java/com/aronkatona/SpringbootJmsApplication.java
@@ -0,0 +1,56 @@
+package com.aronkatona;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.jms.ConnectionFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.config.SimpleJmsListenerContainerFactory;
+import org.springframework.util.FileSystemUtils;
+
+@SpringBootApplication
+@EnableJms
+public class SpringbootJmsApplication {
+
+	@Bean // Strictly speaking this bean is not necessary as boot creates a default
+    JmsListenerContainerFactory<?> myJmsContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleJmsListenerContainerFactory factory = new SimpleJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        return factory;
+    }
+	
+    public static void main(String[] args) {
+        FileSystemUtils.deleteRecursively(new File("activemq-data"));
+
+        ConfigurableApplicationContext context = SpringApplication.run(SpringbootJmsApplication.class, args);
+
+        for (int i = 0; i < 10; i++) {
+        	Sender.sendStringToDestination(context,Destinations.STRING_DESTINATION_A,"heyhooo destination-a: " + i);
+            Sender.sendStringToDestination(context,Destinations.STRING_DESTINATION_B,"heyhooo destination-b: " + i);
+		}
+        
+        Map<String, String> message = new HashMap<String, String>();
+	    message.put("A", "A");
+	    message.put("B", "B");
+	    message.put("C", "C");
+        Sender.sendObjectToDestination(context, Destinations.MAP_DESTINATION_A,message);
+        
+        
+        try {
+			Thread.sleep(4000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+        
+        context.close();
+    	FileSystemUtils.deleteRecursively(new File("activemq-data"));   
+    }
+
+}
